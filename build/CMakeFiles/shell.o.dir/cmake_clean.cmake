9c860345d2b1d85de5610585f2eec5e01a2d56bc
auduongtansang <1712145@student.hcmus.edu.vn>
2019-09-27 19:51:03 +0700

    Build and execute application with cmake

---
+++ build/CMakeFiles/shell.o.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shell.o.dir/shell.c.o"
+  "shell.o.pdb"
+  "shell.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/shell.o.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
